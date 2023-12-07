7c1c5ca36734b53d48907e146b583a1146d84b84
huadong.yang <huadong.yang@qizhidao.com>
2023-12-07 10:30:10 +0800

    feat: 重新规划module分类

--- spring-data-demo/spring-data-multiple-datasource/src/test/java/service/DbTransactionServiceTest.java
+++ spring-data-demo/spring-data-multiple-datasource/src/test/java/service/DbTransactionServiceTest.java
@@ -1,12 +1,15 @@
-package com.birdsnail.multiple.service;
+package service;
 
 import com.birdsnail.multiple.base.City;
 import com.birdsnail.multiple.base.EcLabel;
+import com.birdsnail.multiple.service.DbService;
+import com.birdsnail.multiple.service.DbTransactionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class DbTransactionServiceTest {
